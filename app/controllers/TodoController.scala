25a6d5227ba54b1c3f7298d960a65636581e6afc
lecaoquochung <lecaoquochung@gmail.com>
2020-09-09 16:54:04 +0900

    todo services

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -6,6 +6,7 @@ import play.api.mvc._
 
 import play.api.data._
 import play.api.data.Forms._
+import services._
 //コピペ
 
 //入力
@@ -16,9 +17,14 @@ class TodoController @Inject()(mcc: MessagesControllerComponents)
     Ok("Hello World")
   }
 
+//  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+//    val message: String = "Show TODO list here"
+//    Ok(views.html.list(message))
+//  }
+
   def list() = Action { implicit request: MessagesRequest[AnyContent] =>
-    val message: String = "Show TODO list here"
-    Ok(views.html.list(message))
+    val items: Seq[Todo] = Seq(Todo("Todo1"), Todo("Todo2"))
+    Ok(views.html.list(items))
   }
 
 }
---
+++ app/services/Todo.scala
@@ -0,0 +1,3 @@
+package services
+
+case class Todo(name: String)
