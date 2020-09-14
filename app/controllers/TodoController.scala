b2567e5a5cfc8f4eb3c8c02374aad216d9b008e7
lecaoquochung <lecaoquochung@gmail.com>
2020-09-14 18:58:56 +0900

    TODO CRUD

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -41,10 +41,37 @@ class TodoController @Inject()(todoService: TodoService, mcc: MessagesController
     Ok(views.html.createForm(todoForm))
   }
 
+//  def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
+//    val name: String = todoForm.bindFromRequest().get
+//    println(name)
+//    Ok("Save")
+//  }
+//  def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
+//    val name: String = todoForm.bindFromRequest().get
+//    todoService.insert(Todo(name))
+//    Redirect(routes.TodoController.list())
+//  }
   def todoAdd() = Action { implicit request: MessagesRequest[AnyContent] =>
     val name: String = todoForm.bindFromRequest().get
-    println(name)
-    Ok("Save")
+    todoService.insert(Todo(id = None, name))
+    Redirect(routes.TodoController.list())
+  }
+
+  def todoEdit(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    todoService.findById(todoId).map { todo =>
+      Ok(views.html.editForm(todoId, todoForm.fill(todo.name)))
+    }.getOrElse(NotFound)
+  }
+
+  def todoUpdate(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    todoService.update(todoId, Todo(Some(todoId), name))
+    Redirect(routes.TodoController.list())
+  }
+
+  def todoDelete(todoId: Long) = Action { implicit request: MessagesRequest[AnyContent] =>
+    todoService.delete(todoId)
+    Redirect(routes.TodoController.list())
   }
 
 }
--- app/services/Todo.scala
+++ app/services/Todo.scala
@@ -8,16 +8,23 @@ import play.api.db.DBApi
 
 import scala.language.postfixOps
 
-case class Todo(name: String)
+//case class Todo(name: String)
+case class Todo(id:Option[Long], name: String)
 
 @javax.inject.Singleton
 class TodoService @Inject() (dbapi: DBApi) {
 
   private val db = dbapi.database("default")
 
+//  val simple = {
+//    get[String]("todo.name") map {
+//      case name => Todo(name)
+//    }
+//  }
   val simple = {
-    get[String]("todo.name") map {
-      case name => Todo(name)
+    get[Option[Long]]("todo.id") ~
+      get[String]("todo.name") map {
+      case id~name => Todo(id, name)
     }
   }
 
@@ -33,6 +40,45 @@ class TodoService @Inject() (dbapi: DBApi) {
 
     }
 
+  }
+
+  def insert(todo: Todo) = {
+    db.withConnection { implicit connection =>
+      SQL(
+        """
+        insert into todo values ((select next value for todo_seq), {name})
+        """
+      ).on(
+        'name -> todo.name
+      ).executeUpdate()
+    }
+  }
+
+  def findById(id: Long): Option[Todo] = {
+    db.withConnection { implicit connection =>
+      SQL("select * from todo where id = {id}").on('id -> id).as(simple.singleOpt)
+    }
+  }
+
+  def update(id: Long, todo: Todo) = {
+    db.withConnection { implicit connection =>
+      SQL(
+        """
+          update todo
+          set name = {name}
+          where id = {id}
+        """
+      ).on(
+        'id -> id,
+        'name -> todo.name
+      ).executeUpdate()
+    }
+  }
+
+  def delete(id: Long) = {
+    db.withConnection { implicit connection =>
+      SQL("delete from todo where id = {id}").on('id -> id).executeUpdate()
+    }
   }
 
 }
