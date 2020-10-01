fbf093e92198ee3c4f17131135d72c9681ffdf32
Le Hung (レフン) <lecaoquochung@gmail.com>
2020-10-01 23:05:04 +0900

    init circleci

---
+++ .circleci/qa.sh
@@ -0,0 +1,33 @@
+# Return immediately with an error code if any of the following commands fail
+set -ex
+
+# Install the dependencies for testing with Node
+yarn install
+
+# Start the scala server in the background
+# screen -d -m -S api /root/sbt/bin/sbt -Dconfig.file=conf/application.conf -Dplay.evolutions.db.default.autoApply=true "run 80"
+# curl --retry 60 --retry-connrefused --retry-max-time 60 --retry-delay 1 http://127.0.0.1
+screen -d -m -S api sbt \
+           -Dconfig.file=conf/application.conf \
+           -Dplay.evolutions.db.default.autoApply=true \
+           "run 80"
+
+# Wait for the server to start up
+curl --retry 60 \
+     --retry-connrefused \
+     --retry-max-time 60 \
+     --retry-delay 1 \
+     http://api.test
+
+# API server health check
+curl --retry 60 \
+     --retry-connrefused \
+     --retry-max-time 60 \
+     --retry-delay 1 \
+     http://api.test/api/health
+
+# Run tests
+# cd /home/qa/; ./help.sh cli test cucumber local tag "@api"
+
+# Kill the background server
+screen -S api -X quit
